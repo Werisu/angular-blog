70f9b8364174507ad58a527f09775854eebf6298
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-03-21 13:44:26 -0300

    :rocket:

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import Typewriter from 'src/app/animations/typewriter';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
   styleUrls: ['./menu-title.component.css']
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements AfterViewInit {
+  @ViewChild('typingText') typingText!: ElementRef<any>;
+  public typewriter: any;
 
+  ngAfterViewInit(): void {
+    console.log(this.typingText);
+    this.typewriter = new Typewriter(this.typingText.nativeElement, {
+      loop: true,
+      color: 'red'
+    });
+    //inovadoras e personalizadas
+    this.typewriter
+      .typeString('notícias legais')
+      .pauseFor(4000)
+      .deleteChars(14)
+      .typeString('diversão garantida')
+      .pauseFor(4000)
+      .deleteChars(17)
+      .typeString('resenhas de filmes')
+      .pauseFor(4000)
+      .deleteChars(17)
+      .typeString('opinião e discussões')
+      .pauseFor(5000)
+      .deleteAll(10)
+      .start();
+  }
 }
